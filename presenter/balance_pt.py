328eb21cc311f147177bfcc0a83b969c2d835693
zhao <mephisto512@aliyun.com>
2021-03-24 21:29:12 +0800

    调整代码结构

--- presenter/balance_pt.py
+++ presenter/balance_pt.py
@@ -1,10 +1,23 @@
 from presenter.dash_app import *
 from dash.dependencies import Input, Output
 from constants.constant import *
+import dash_core_components as dcc
 import plotly.express as px
 
 
 class BalancePt(object):
+    def set_layout(self, df):
+        add_layout([
+            dcc.RangeSlider(
+                id='balance_slider',
+                min=df[BALANCE_COL_START].min().year,
+                max=df[BALANCE_COL_START].max().year,
+                value=[df[BALANCE_YEAR].min(), df[BALANCE_YEAR].max()],
+                marks={str(ymd): str(ymd) for ymd in df[BALANCE_YEAR].unique()},
+            ),
+            dcc.Graph(id="balance"),
+        ])
+
     def draw(self, df):
         @app.callback(Output("balance", "figure"), [Input("balance_slider", "value")])
         def monetary_fund_chart(date):
--- usecase/balance/balance_uc.py
+++ usecase/balance/balance_uc.py
@@ -1,7 +1,4 @@
 from repository.balance_repo import BalanceSheet
-from constants.constant import *
-import dash_core_components as dcc
-from presenter.dash_app import *
 import pandas as pd
 
 
@@ -18,21 +15,7 @@ class BalanceUc(object):
         pass
 
     def __set_layout(self):
-        add_layout([
-            dcc.RangeSlider(
-                id='balance_slider',
-                min=self.__df[BALANCE_COL_START].min().year,
-                max=self.__df[BALANCE_COL_START].max().year,
-                value=[self.__df[BALANCE_YEAR].min(), self.__df[BALANCE_YEAR].max()],
-                marks={str(ymd): str(ymd) for ymd in self.__df[BALANCE_YEAR].unique()},
-            ),
-            # dcc.Dropdown(
-            #     id='dataframe',
-            #     options=[{'label': col, 'value': self.__df[[BALANCE_COL_START, col]]} for col in self.__repo.get_column()],
-            #     value=self.__df[[BALANCE_COL_START, BALANCE_DEFAULT_DRAW]]
-            # ),
-            dcc.Graph(id="balance"),
-        ])
+        self.__prt.set_layout(self.__df)
 
     def run(self):
         self.__get_data()
