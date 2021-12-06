3b5294b2b91b9342e9eaf0c3dc244853d87b120e
zhao <mephisto512@aliyun.com>
2021-12-07 00:01:50 +0800

    财务数据demo

--- demo/test_demo.py
+++ demo/test_demo.py
@@ -1,48 +1,80 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+from bs4 import BeautifulSoup
+import requests
 
-from dash import Dash
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
+URL = 'https://caibaoshuo.com/companies/002020/financials'
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-markdown_text = '''
-### Dash and Markdown
-
-Dash apps can be written in Markdown.
-Dash uses the [CommonMark](http://commonmark.org/)
-specification of Markdown.
-Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
-if this is your first introduction to Markdown!
-'''
-
-all_dims = ['sepal_length', 'sepal_width',
-            'petal_length', 'petal_width']
-
-app.layout = html.Div([
-    dcc.Markdown(children=markdown_text),
-    dcc.Dropdown(
-        id="dropdown",
-        options=[{"label": x, "value": x} for x in all_dims],
-        value=all_dims[:2],
-        multi=True
-    ),
-    dcc.Graph(id="splom"),
-])
-
-df = px.data.iris()
+COL = [
+    '现金与约当现金',
+    '现金流量比率',
+    '现金流量允当比率',
+    '现金再投资比率',
+    '总资产周转率',
+    '应收账款周转天数',
+    '存货周转天数',
+    '净资产收益率',
+    '毛利率',
+    '营业利润率',
+    '经营安全边际率',
+    '资产负债率',
+    '长期资金占重资产比率',
+    '流动比率',
+    '速动比率',
+]
 
 
-@app.callback(Output("splom", "figure"), [Input("dropdown", "value")])
-def update_bar_chart(dims):
-    fig = px.scatter_matrix(df, dimensions=dims, color="species")
-    return fig
+# def table_data(table):
+#     rows = []
+#     trs = table.find_all('tr')
+#     headerow = [td.get_text(strip=True) for td in trs[0].find_all('th')]
+#     if headerow:
+#         rows.append(headerow)
+#         trs = trs[1:]
+#     for tr in trs:
+#         tds = []
+#         for td in tr.find_all('td'):
+#             t = td.find_all('span')
+#             if len(t) > 1:
+#                 tds.extend(t[1])
+#             else:
+#                 tds.append(td.get_text(strip=True))
+#         rows.append(tds)
+#     return rows
+
+
+def get_table_data(table):
+    trs = table.find_all('tr')
+    rs = []
+    for c in COL:
+        for tr in trs:
+            if c not in tr.get_text():
+                continue
+
+            each = []
+            tmp = []
+            for td in tr.find_all('td'):
+                t = td.find_all('span')
+                if len(t) > 1:
+                    each.extend(t[1])
+                else:
+                    d = td.get_text(strip=True)
+                    each.append(d if '--' not in d else '999')
+            tmp.append(each[0])
+            tmp.extend(each[-5:])
+            rs.append(tmp)
+    return rs
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    proxies = {"http": None, "https": None}
+    r = requests.get(URL, verify=False, proxies=proxies)
+    soup = BeautifulSoup(r.text, 'lxml')
+    table_one = get_table_data(soup.find(id='albs-yearly').find('table'))
+
+    table_two = get_table_data(soup.find(id='alkey-yearly').find('table'))
+    table_one.extend(table_two)
+
+    import pandas as pd
+    col = ['类别', '2017', '2018', '2019', '2020', '近12个月']
+    df = pd.DataFrame(table_one, columns=col)
+    df[col[1:]] = df[col[1:]].astype(float)
+    print(df)
